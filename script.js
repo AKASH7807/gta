0894f6b17ac7a321eea298887a51a19eb55c1aca
unknown <dreamhouse7807@gmail.com>
2024-11-06 22:02:17 +0530

    version 1.0

---
+++ script.js
@@ -0,0 +1,71 @@
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+}
+
+//phone card
+ScrollReveal().reveal(".logo", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".icons", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+ScrollReveal().reveal(".info", {
+    ...scrollRevealOption,
+    origin: "bottom",
+    scale: 0.7,
+});
+
+ScrollReveal().reveal(".btns", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".gang", {
+    ...scrollRevealOption,
+    origin: "bottom",
+    scale: 0.7,
+});
+
+ScrollReveal().reveal(".title", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".guns", {
+    ...scrollRevealOption,
+    origin: "left",
+    scale: 0.7,
+});
+
+ScrollReveal().reveal(".group", {
+    ...scrollRevealOption,
+    origin: "top",
+    scale: 0.7,
+});
+
+ScrollReveal().reveal(".card", {
+    ...scrollRevealOption,
+    origin: "bottom",
+    scale: 0.7,
+});
+
+ScrollReveal().reveal(".links", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".bottom", {
+    ...scrollRevealOption,
+    origin: "bottom",
+});
+ScrollReveal().reveal(".card-info", {
+    ...scrollRevealOption,
+    origin: "bottom",
+});
+ScrollReveal().reveal(".buy-card", {
+    ...scrollRevealOption,
+    origin: "left",
+});
